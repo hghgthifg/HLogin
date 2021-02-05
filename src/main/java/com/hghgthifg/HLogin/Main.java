9c1d6835baada6d312d02dd634bd5a85a262767a
hghgthifg <3354437830@qq.com>
2021-02-05 19:29:07 +0800

    增加了创建数据文件的功能

--- src/main/java/com/hghgthifg/HLogin/Main.java
+++ src/main/java/com/hghgthifg/HLogin/Main.java
@@ -8,9 +8,12 @@ import org.bukkit.Bukkit;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.io.File;
+
 public class Main extends JavaPlugin{
     // 私有实例
     private FileConfiguration config;
+    private File userDataFolder=new File(getDataFolder(),"userdata");
 
     /*
      * 插件关闭时的行为
@@ -31,10 +34,12 @@ public class Main extends JavaPlugin{
         //获取config信息
         if (!getDataFolder().mkdir()) {
             saveDefaultConfig();
+            if (!userDataFolder.mkdir()){
+                getLogger().info("Create userdata folder");
+            }
             reloadConfig();
         }
         config = getConfig();
-
         //注册命令
         Bukkit.getPluginCommand("login").setExecutor(new LoginCommand());
         getLogger().info("Command login loaded");
@@ -42,7 +47,7 @@ public class Main extends JavaPlugin{
         getLogger().info("Command register loaded");
 
         //注册监听器
-        Bukkit.getPluginManager().registerEvents(new LoginEvent(),this);
+        Bukkit.getPluginManager().registerEvents(new LoginEvent(userDataFolder,getLogger()),this);
         getLogger().info("Listener login loaded");
     }
 
--- src/main/java/com/hghgthifg/HLogin/event/LoginEvent.java
+++ src/main/java/com/hghgthifg/HLogin/event/LoginEvent.java
@@ -4,11 +4,34 @@ import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerLoginEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Logger;
 
 public final class LoginEvent implements Listener {
+    private final File userDataFolder;
+    private final Logger logger;
+    private File userData;
+
+    public LoginEvent(File Folder,Logger logger){
+        this.userDataFolder=Folder;
+        this.logger=logger;
+    }
+
     @EventHandler
-    public void onLogin(PlayerLoginEvent event) {
-        event.getPlayer().sendMessage("Hello,"+event.getPlayer().getName()+".");
+    public void onLogin(PlayerJoinEvent event) {
+        userData=new File(userDataFolder,event.getPlayer().getUniqueId().toString());
+        if (!userData.exists()){
+            try {
+                if (userData.createNewFile()){
+                    logger.info("为"+event.getPlayer().getName()+"创建了数据文件");
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        event.getPlayer().sendMessage("Hello "+event.getPlayer().getName()+"!");
     }
 }
